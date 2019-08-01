f6eade489c4daf7ee4364d2fb5f6334158cac85b
George Daramouskas <gdaramouskas@therp.nl>
2019-08-01 17:13:32 +0200

    [FIX] [base_view_inheritance_extension When a user is deactivated, deactivate their view, when deleted, delete their view

---
+++ base_view_inheritance_extension/models/res_users.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openerp import api, models
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    @api.multi
+    def unlink(self):
+        """
+        Finds any views assigned specifically to this user and deletes them.
+        """
+        self.env['ir.ui.view'].sudo().with_context(
+            active_test=False).search([
+                ('user_ids', 'in', self.ids)]).filtered(
+                    lambda x: len(x.user_ids) == 1).unlink()
+        return super(ResUsers, self).unlink()
+
+    @api.multi
+    def write(self, vals):
+        """
+        If a user has been set as inactive then, any views that are assigned
+        to them specifically as set as inactive as well.
+        """
+        active = vals.get('active')
+        if active is not None:
+            self.env['ir.ui.view'].sudo().with_context(
+                active_test=False).search([
+                    ('user_ids', 'in', self.ids)]).filtered(
+                        lambda x: len(x.user_ids) == 1).write({
+                            'active': active})
+        return super(ResUsers, self).write(vals)
